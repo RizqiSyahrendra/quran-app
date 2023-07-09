18cae4f8bbce63a7e41af65b91c351da8007b5b7
RizqiSyahrendra <rizqialaudinsyahrendra@gmail.com>
2023-07-09 12:25:40 +0700

    Fix layout fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import { Poppins } from 'next/font/google'
 import Providers from './providers'
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700'] })
+const poppins = Poppins({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] })
 
 export const metadata = {
   title: 'Quran App',
