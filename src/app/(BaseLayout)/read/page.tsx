66d6bb635000af71d3dc4e326bb667a3b5d4dfea
RizqiSyahrendra <rizqialaudinsyahrendra@gmail.com>
2023-07-09 22:05:14 +0700

    get searchParams read page

--- src/app/(BaseLayout)/read/page.tsx
+++ src/app/(BaseLayout)/read/page.tsx
@@ -6,12 +6,18 @@ import HeaderContainerAyat from "@/components/module/read/HeaderContainerAyat";
 import { useChaptersData } from "@/utils/hooks/data/useChaptersData";
 import { Fragment, useState } from "react";
 
-export default function Read() {
+export default function Read({
+    searchParams
+}: {
+    searchParams: { surat: string, juz: string }
+}) {
     const [page, setPage] = useState(1)
     const { chapters, isLoadingChapters } = useChaptersData()
     const [chapterNum, setChapterNum] = useState<number>(0)
     const [juzNum, setJuzNum] = useState<number>(0)
-    
+
+    console.log(searchParams);
+
     const onVersesDataChanged: IContainerAyatProps["onDataChanged"] = (verses) => {
         if (verses?.length > 0) {
             let latestVerse = verses[verses.length - 1];
